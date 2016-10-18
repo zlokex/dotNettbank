b7d2ae3db94d7a38606014bc672b2175bea927c0
zlokex <andre.hovda@gmail.com>
2016-10-18 18:07:47 +0200

    Added a couple methods in DAL AccountRepo

--- dotNettbank/DAL/Repositories/AccountRepository.cs
+++ dotNettbank/DAL/Repositories/AccountRepository.cs
@@ -10,9 +10,29 @@ namespace dotNettbank.DAL.Repositories
     {
         BankContext db = new BankContext();
 
-        public List<Account> getAllAccounts()
+        public List<Account> getAll()
         {
-            return null;
+            return db.Accounts.ToList();
+        }
+
+        public Account getByAccountNo(int accountNo)
+        {
+            return db.Accounts.FirstOrDefault(a => a.AccountNo == accountNo);
+        }
+
+        public List<Account> getByType(AccountType type)
+        {
+            return db.Accounts.Where(a => a.Type == type).ToList();
+        }
+
+        public List<Account> getByBirthNo(string birthNo)
+        {
+            return db.Accounts.Where(a => a.Owner.BirthNo == birthNo).OrderBy(a => a.Type).ToList();
+        }
+
+        public List<Account> getByTypeAndBirthNo(AccountType type, string birthNo)
+        {
+            return db.Accounts.Where(a => a.Type == type && a.Owner.BirthNo == birthNo).ToList();
         }
     }
 }
