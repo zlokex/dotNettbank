8517609f944234c0de3c81a3f939ca3298d0c1c9
Magnus Bärnholt <magnus.barnholt@me.com>
2016-10-18 20:58:17 +0200

    Fikset partialview på login og fikset litt på layout for innlogget og utlogget samt index.cshtml

---
+++ dotNettbank/dotNettbank/Models/signin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace dotNettbank.Models
+{
+    public class signin
+    {
+        [Required(ErrorMessage = "Fødselsnummer må oppgis")]
+        [Display(Name = "Fødeselsnummer")]
+        public string BirthNo { get; set; }
+
+        [Required(ErrorMessage = "Passord må oppgis")]
+        [Display(Name = "Passord")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
