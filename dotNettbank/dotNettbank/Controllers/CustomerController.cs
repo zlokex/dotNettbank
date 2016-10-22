b83e659e7d54e76c849acad689b7915cdc706241
zlokex <andre.hovda@gmail.com>
2016-10-22 20:53:42 +0200

    Removed Login from Customer View/Controller

--- dotNettbank/dotNettbank/Controllers/CustomerController.cs
+++ dotNettbank/dotNettbank/Controllers/CustomerController.cs
@@ -20,45 +20,6 @@ namespace dotNettbank.Controllers
             return View();
         }
 
-        
-        public ActionResult Login()
-        {
-            // vis innlogging
-            if (Session["LoggedIn"] == null)
-            {
-                Session["LoggedIn"] = false;
-                ViewBag.LoggedIn = false;
-            }
-            else
-            {
-                ViewBag.LoggedIn = (bool)Session["LoggedIn"];
-            }
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Login(LoginViewModel loginCredentials)
-        {
-            string password = loginCredentials.Password;
-            string birthNo = loginCredentials.BirthNo;
-
-            // sjekk om innlogging OK
-            if (bankService.checkValidLogin(password, birthNo))
-            {
-                // Ja brukernavn og passord er OK!
-                Session["LoggedIn"] = true;
-                ViewBag.LoggedIn = true;
-                return RedirectToAction("Overview");
-            }
-            else
-            {
-                // Nei brukernavn og passord er IKKE OK!
-                Session["LoggedIn"] = false;
-                ViewBag.LoggedIn = false;
-                return View();
-            }
-        }
-
 
         public ActionResult Overview() // Total oversikt
         {
