8cb2d2128eb5cf3ed06b57509db8841157237e95
zlokex <andre.hovda@gmail.com>
2016-11-07 21:47:01 +0100

    Endre konto now works, including validation messages (only for required fields)

--- dotNettbank/dotNettbankAdmin/Models/AccountVM.cs
+++ dotNettbank/dotNettbankAdmin/Models/AccountVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,20 @@ namespace dotNettbankAdmin.Models
 {
     public class AccountVM
     {
+        [Required(ErrorMessage = "Eier kan ikke være blankt")]
+        [Display(Name = "Eier")]
         public string OwnerBirthNo { get; set; }
+
+        [Required(ErrorMessage = "Kontonummer kan ikke være blankt")]
+        [Display(Name = "Kontonummer")]
         public string AccountNo { get; set; } //kontonr
+
+        [Required(ErrorMessage = "Balanse kan ikke være blankt")]
+        [Display(Name = "Balanse")]
         public double Balance { get; set; } //Saldo
+
+        [Required(ErrorMessage = "Kontonavn kan ikke være blankt")]
+        [Display(Name = "Kontonavn")]
         public string Type { get; set; } // Kontotype
     }
 }
